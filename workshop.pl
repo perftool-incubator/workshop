8fa33f3948ef769cf460defb4e6132168ecceb4c
Karl Rister <krister@redhat.com>
2020-02-07 09:25:22 -0600

    workshop.pl: implement logging infrastructure
    - Add the --log-level=<level> argument.  Current available values are:
    
      info <default>
      verbose
      debug
    
    - Centralize log formatting in the new 'logger' function.
    
    - Print a bunch of output from the forked programs under the 'verbose'
      log level.
    
    - Print dumps of the data structures under the 'debug' log level.

--- workshop.pl
+++ workshop.pl
@@ -11,6 +11,64 @@ use Data::Dumper;
 my $me = "workshop";
 
 my %args;
+$args{'log-level'} = 'info';
+
+sub logger {
+    my ($log_level, $log_msg) = @_;
+
+    my $log_it = 0;
+    my $prefix = "";
+
+    if (($log_level eq 'debug') &&
+	($args{'log-level'} eq 'debug')) {
+	$log_it = 1;
+	$prefix = "[DEBUG] ";
+    } elsif (($log_level eq 'verbose') &&
+	     (($args{'log-level'} eq 'debug') ||
+	      ($args{'log-level'} eq 'verbose'))) {
+	$log_it = 1;
+	$prefix = "[VERBOSE] ";
+    } elsif (($log_level eq 'info') &&
+	     (($args{'log-level'} eq 'debug') ||
+	      ($args{'log-level'} eq 'verbose') ||
+	      ($args{'log-level'} eq 'info'))) {
+	$log_it = 1;
+    } elsif ($log_level eq 'error') {
+	$log_it = 1;
+	$prefix = "[ERROR] ";
+    }
+
+    if (!$log_it) {
+	return;
+    }
+
+    my $add_newline = 0;
+    if ($log_msg =~ /\n$/) {
+	$add_newline = 1;
+    }
+
+    my @lines = split(/\n/, $log_msg);
+
+    if ($log_level eq 'error') {
+	select(STDERR);
+    }
+
+    my $line_idx = 0;
+    for ($line_idx=0; $line_idx<(scalar(@lines) - 1); $line_idx++) {
+	print $prefix . $lines[$line_idx] . "\n";
+    }
+
+    if ($add_newline) {
+	print $prefix . $lines[$line_idx] . "\n";
+    } else {
+	print $prefix . $lines[$line_idx];
+    }
+
+    if ($log_level eq 'error') {
+	select(STDOUT);
+    }
+
+}
 
 sub arg_handler {
     my ($opt_name, $opt_value) = @_;
@@ -33,18 +91,30 @@ sub arg_handler {
 	push(@{$args{'reqs'}}, $opt_value);
     } elsif ($opt_name eq "skip-update") {
 	$args{'skip-update'} = 1;
+    } elsif ($opt_name eq "log-level") {
+	if (($opt_value eq 'info') ||
+	    ($opt_value eq 'verbose') ||
+	    ($opt_value eq 'debug')) {
+	    $args{'log-level'} = $opt_value;
+	} else {
+	    die("--log-level must be one of 'info', 'verbose', or 'debug' [not '$opt_value']\n");
+	}
+    } else {
+	die("I'm confused, how did I get here [$opt_name]?");
     }
 }
 
-GetOptions("requirements=s" => \&arg_handler,
+GetOptions("log-level=s" => \&arg_handler,
+	   "requirements=s" => \&arg_handler,
 	   "skip-update" => \&arg_handler,
 	   "target=s" => \&arg_handler)
     or die("Error in command line arguments");
 
-#print Dumper \%args;
+logger('debug', "Argument Hash:\n");
+logger('debug', Dumper(\%args));
 
 if (! exists $args{'target'}) {
-    print STDERR "ERROR: You must provide --target!\n";
+    logger('error', "You must provide --target!\n");
     exit 1;
 }
 
@@ -60,14 +130,14 @@ if (open(my $target_fh, "<", $args{'target'})) {
 
     close($target_fh);
 } else {
-    print STDERR "ERROR: Could not open target file '$args{'target'}' for reading!\n";
+    logger('error', "Could not open target file '$args{'target'}' for reading!\n");
     exit 2;
 }
 
 my @all_requirements;
 my %active_requirements;
 
-print "Processing requested requirements...";
+logger('info', 'Processing requested requirements...');
 
 foreach my $req (@{$args{'reqs'}}) {
     if (open(my $req_fh, "<", $req)) {
@@ -83,13 +153,14 @@ foreach my $req (@{$args{'reqs'}}) {
 
 	close($req_fh);
     } else {
-	print "failed\n";
-	print STDERR "ERROR: Failed to load requirement file '$req'!\n";
+	logger('info', "failed\n");
+	logger('error', "Failed to load requirement file '$req'!\n");
 	exit 21;
     }
 }
 
-#print Dumper \@all_requirements;
+logger('debug', "All Requirements Hash:\n");
+logger('debug', Dumper(\@all_requirements));
 
 $active_requirements{'hash'} = ();
 $active_requirements{'array'} = [];
@@ -102,15 +173,15 @@ foreach my $tmp_req (@all_requirements) {
     } elsif (exists($tmp_req->{'json'}{'targets'}{'default'})) {
 	$target_label = 'default';
     } else {
-	print "failed\n";
-	print STDERR "ERROR: Could not find appropriate target match in requirements '$tmp_req->{'name'}' for '$target_json->{'label'}'!\n";
+	logger('info', "failed\n");
+	logger('error', "Could not find appropriate target match in requirements '$tmp_req->{'name'}' for '$target_json->{'label'}'!\n");
 	exit 22;
     }
 
     for my $target_package (@{$tmp_req->{'json'}{'targets'}{$target_label}{'packages'}}) {
 	if (exists($active_requirements{'hash'}{$target_package})) {
-	    print "failed\n";
-	    print STDERR "ERROR: There is a target package conflict between '$tmp_req->{'name'}' and '$active_requirements{'hash'}{$target_package}{'requirement_source'}' for '$target_package'!\n";
+	    logger('info', "failed\n");
+	    logger('error', "There is a target package conflict between '$tmp_req->{'name'}' and '$active_requirements{'hash'}{$target_package}{'requirement_source'}' for '$target_package'!\n");
 	    exit 23;
 	}
 
@@ -122,9 +193,10 @@ foreach my $tmp_req (@all_requirements) {
 }
 
 # requirements processing end
-print "succeeded\n";
+logger('info', "succeeded\n");
 
-#print Dumper \%active_requirements;
+logger('debug', "Active Requirements Hash:\n");
+logger('debug', Dumper(\%active_requirements));
 
 my $buildah_output;
 my $rc;
@@ -135,85 +207,111 @@ my $container_mount_point;
 $buildah_output = `buildah images --json $target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'} 2>&1`;
 $rc = $? >> 8;
 if ($rc == 0) {
-    print "Found $target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'} locally\n";
+    logger('info', "Found $target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'} locally\n");
+    logger('verbose', $buildah_output);
     $target_json->{'source'}{'local_details'} = decode_json($buildah_output);
 } else {
-    print "Could not find $target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'}, attempting to download\n";
+    logger('info', "Could not find $target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'}, attempting to download...");
+    logger('verbose', $buildah_output);
     $buildah_output = `buildah pull --quiet $target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'} 2>&1`;
     $rc = $? >> 8;
     if ($rc == 0) {
-	print "Download succeeded\n";
+	logger('info', "succeeded\n");
+	logger('verbose', $buildah_output);
+
+	logger('info', "Querying for information about the image...");
 	$buildah_output = `buildah images --json $target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'} 2>&1`;
 	$rc = $? >> 8;
 	if ($rc == 0) {
+	    logger('info', "succeeded\n");
+	    loggeR('verbose', $buildah_output);
 	    $target_json->{'source'}{'local_details'} = decode_json($buildah_output);
 	} else {
-	    print STDERR "ERROR: Failed to download/query $target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'}!\n";
-	    print STDERR "       output: $buildah_output\n";
+	    logger('info', "failed\n");
+	    logger('error', $buildah_output);
+	    logger('error', "Failed to download/query $target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'}!\n");
 	    exit 3;
 	}
     } else {
-	print STDERR "ERROR: Failed to download $target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'}!\n";
-	print STDERR "       output: $buildah_output\n";
+	logger('info', "failed\n");
+	logger('error', $buildah_output);
+	logger('error', "Failed to download $target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'}!\n");
 	exit 4;
     }
 }
 
-#print Dumper $target_json;
+logger('debug', "Target JSON:\n");
+logger('debug', Dumper($target_json));
 
 my $tmp_container = $me . "_" . $target_json->{'label'};
 
 # make sure there isn't an old container hanging around
+logger('info', "Checking for stale container presence...");
 $buildah_output = `buildah containers --filter name=$tmp_container --json 2>&1`;
 $rc = $? >> 8;
 if ($buildah_output !~ /null/) {
+    logger('info', "found\n");
+    logger('verbose', $buildah_output);
+
     # need to clean up an old container
-    print "Cleaning up old container...";
+    logger('info', "Cleaning up old container...");
     $buildah_output = `buildah rm $tmp_container 2>&1`;
     $rc = $? >> 8;
     if ($rc != 0) {
-	print "failed\n";
-	print STDERR "ERROR: Could not clean up old container '$tmp_container'!\n";
-	print STDERR "       output: $buildah_output\n";
+	logger('info', "failed\n");
+	logger('error', $buildah_output);
+	logger('error', "Could not clean up old container '$tmp_container'!\n");
 	exit 5;
     } else {
-	print "succeeded\n";
+	logger('info', "succeeded\n");
+	logger('verbose', $buildah_output);
     }
+} else {
+    logger('info', "not found\n");
+    logger('verbose', $buildah_output);
 }
 
 # cleanup an existing container image that we are going to replace, if it exists
+logger('info', "Checking if container image already exists...");
 $buildah_output = `buildah images --json $tmp_container 2>&1`;
 $rc = $? >> 8;
 if ($rc == 0) {
-    print "Removing existing container image that I am about to replace [$tmp_container]...";
+    logger('info', "found\n");
+    logger('verbose', $buildah_output);
+    logger('info', "Removing existing container image that I am about to replace [$tmp_container]...");
     $buildah_output = `buildah rmi $tmp_container 2>&1`;
     $rc = $? >> 8;
     if ($rc != 0) {
-	print "failed\n";
-	print STDERR "ERROR: Could not remove existing container image '$tmp_container'!\n";
-	print STDERR "       output: $buildah_output\n";
+	logger('info', "failed\n");
+	logger('error', $buildah_output);
+	logger('error', "Could not remove existing container image '$tmp_container'!\n");
 	exit 11;
     } else {
-	print "succeeded\n";
+	logger('info', "succeeded\n");
+	logger('verbose', $buildah_output);
     }
+} else {
+    logger('info', "not found\n");
+    logger('verbose', $buildah_output);
 }
 
 # create a new container based on the target source
-print "Creating temporary container...";
+logger('info', "Creating temporary container...");
 $buildah_output = `buildah from --name $tmp_container $target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'} 2>&1`;
 $rc = $? >> 8;
 if ($rc != 0) {
-    print "failed\n";
-    print STDERR "ERROR: Could not create new container '$tmp_container' from '$target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'}'!\n";
-    print STDERR "       output: $buildah_output\n";
+    logger('info', "failed\n");
+    logger('error', $buildah_output);
+    logger('error', "Could not create new container '$tmp_container' from '$target_json->{'source'}{'image'}:$target_json->{'source'}{'tag'}'!\n");
     exit 6;
 } else {
-    print "succeeded\n";
+    logger('info', "succeeded\n");
+    logger('verbose', $buildah_output);
 }
 
 if (!exists($args{'skip-update'})) {
     # update the container's existing content
-    print "Updating the temporary container...";
+
     my $update_cmd = "";
     my $clean_cmd = "";
     if ($target_json->{'packages'}{'manager'} eq "dnf") {
@@ -223,46 +321,54 @@ if (!exists($args{'skip-update'})) {
 	$update_cmd = "yum update --assumeyes";
 	$clean_cmd = "yum clean all";
     }
+
+    logger('info', "Updating the temporary container...");
     $buildah_output = `buildah run $tmp_container -- $update_cmd 2>&1`;
     $rc = $? >> 8;
     if ($rc != 0) {
-	print "failed\n";
-	print STDERR "ERROR: Updating the temporary container '$tmp_container' failed!\n";
-	print STDERR "       output: $buildah_output\n";
+	logger('info', "failed\n");
+	logger('error', $buildah_output);
+	logger('error', "Updating the temporary container '$tmp_container' failed!\n");
 	exit 7;
     } else {
-	$buildah_output = `buildah run $tmp_container -- $clean_cmd 2>&1`;
-	$rc = $? >> 8;
-	if ($rc != 0) {
-	    print "failed\n";
-	    print STDERR "ERROR: Updating the temporary container '$tmp_container' failed because it could not clean up after itself!\n";
-	    print STDERR "       output: $buildah_output\n";
-	    exit 12;
-	} else {
-	    print "succeeded\n";
-	}
+	logger('info', "succeeded\n");
+	logger('verbose', $buildah_output);
+    }
+
+    logger('info', "Cleaning up after the update...");
+    $buildah_output = `buildah run $tmp_container -- $clean_cmd 2>&1`;
+    $rc = $? >> 8;
+    if ($rc != 0) {
+	logger('info', "failed\n");
+	logger('error', $buildah_output);
+	logger('error', "Updating the temporary container '$tmp_container' failed because it could not clean up after itself!\n");
+	exit 12;
+    } else {
+	logger('info', "succeeded\n");
+	logger('verbose', $buildah_output);
     }
 } else {
-    print "Skipping update due to --skip-update\n";
+    logger('info', "Skipping update due to --skip-update\n");
 }
 
 # mount the container image
-print "Mounting the temporary container's fileystem...";
+logger('info', "Mounting the temporary container's fileystem...");
 $buildah_output = `buildah mount $tmp_container 2>&1`;
 $rc = $? >> 8;
 if ($rc != 0) {
-    print "failed\n";
-    print STDERR "ERROR: Failed to mount the temporary container's filesystem!\n";
-    print STDERR "       output: $buildah_output\n";
+    logger('info', "failed\n");
+    logger('error', $buildah_output);
+    logger('error', "Failed to mount the temporary container's filesystem!\n");
     exit 13;
 } else {
-    print "succeeded\n";
+    logger('info', "succeeded\n");
+    logger('verbose', $buildah_output);
     chomp($buildah_output);
     $container_mount_point = $buildah_output;
 }
 
 # what follows is a complicated mess of code to chroot into the
-# temporary container's filesystem.  Once their all kinds of stuff can
+# temporary container's filesystem.  Once there all kinds of stuff can
 # be done to install packages and make other tweaks to the container
 # if necessary.
 
@@ -274,88 +380,92 @@ if (opendir(NORMAL_ROOT, "/")) {
     # jump into the container image
     if (chroot($container_mount_point)) {
 	if (chdir("/root")) {
-	    print "I'm in the container filesystem's /root!\n";
-	    print `ls` . "\n";
+	    logger('info', "I'm in the container filesystem's /root!\n");
+	    logger('info', `ls` . "\n");
 
 	    # break out of the chroot and return to the old path/pwd
 	    if (chdir(*NORMAL_ROOT)) {
 		if (chroot(".")) {
 		    if (!chdir($pwd)) {
-			print STDERR "ERROR: Could not chdir back to the original path/pwd!\n";
+			logger('error', "Could not chdir back to the original path/pwd!\n");
 			exit 20;
 		    }
 		} else {
-		    print STDERR "ERROR: Could not chroot out of the chroot!\n";
+		    logger('error', "Could not chroot out of the chroot!\n");
 		    exit 19;
 		}
 	    } else {
-		print STDERR "ERROR: Could not chdir to escape the chroot!\n";
+		logger('error', "Could not chdir to escape the chroot!\n");
 		exit 18;
 	    }
 	} else {
-	    print STDERR "ERROR: Could not chdir to temporary container mount point [$container_mount_point]!\n";
+	    logger('error', "Could not chdir to temporary container mount point [$container_mount_point]!\n");
 	    exit 17;
 	}
     } else {
-	print STDERR "ERROR: Could not chroot to temporary container mount point [$container_mount_point]!\n";
+	logger('error', "Could not chroot to temporary container mount point [$container_mount_point]!\n");
 	exit 16;
     }
 
     closedir(NORMAL_ROOT);
 } else {
-    print STDERR "ERROR: Could not get directory reference to '/'!\n";
+    logger('error', "Could not get directory reference to '/'!\n");
     exit 15;
 }
 
 # unmount the container image
-print "Unmounting the temporary container's filesystem...";
+logger('info', "Unmounting the temporary container's filesystem...");
 $buildah_output = `buildah unmount $tmp_container 2>&1`;
 $rc = $? >> 8;
 if ($rc != 0) {
-    print "failed\n";
-    print STDERR "ERROR: Failed to unmount the temporary container's filesystem [$container_mount_point]!\n";
-    print STDERR "       output: $buildah_output\n";
+    logger('info', "failed\n");
+    logger('error', $buildah_output);
+    logger('error', "Failed to unmount the temporary container's filesystem [$container_mount_point]!\n");
     exit 14;
 } else {
-    print "succeeded\n";
+    logger('info', "succeeded\n");
+    logger('verbose', $buildah_output);
 }
 
 # create the new container image
-print "Creating new container image...";
+logger('info', "Creating new container image...");
 $buildah_output = `buildah commit --quiet $tmp_container $tmp_container 2>&1`;
 $rc = $? >> 8;
 if ($rc != 0) {
-    print "failed\n";
-    print STDERR "ERROR: Failed to create new container image '$tmp_container'!\n";
-    print STDERR "       output: $buildah_output\n";
+    logger('info', "failed\n");
+    logger('error', $buildah_output);
+    logger('error', "Failed to create new container image '$tmp_container'!\n");
     exit 8;
 } else {
-    print "succeeded\n";
+    logger('info', "succeeded\n");
+    logger('verbose', $buildah_output);
 }
 
 # clean up the temporary container
-print "Cleaning up the temporary container...";
+logger('info', "Cleaning up the temporary container...");
 $buildah_output = `buildah rm $tmp_container 2>&1`;
 $rc = $? >> 8;
 if ($rc != 0) {
-    print "failed\n";
-    print STDERR "ERROR: Failed to cleanup temporary container '$tmp_container'!\n";
-    print STDERR "       output: $buildah_output\n";
+    logger('info', "failed\n");
+    logger('error', $buildah_output);
+    logger('error', "Failed to cleanup temporary container '$tmp_container'!\n");
     exit 9;
 } else {
-    print "succeeded\n";
+    logger('info', "succeeded\n");
+    logger('verbose', $buildah_output);
 }
 
 # give the user information about the new container image
+logger('info', "Creation of container image '$tmp_container' is complete.  Retreiving some details about your new image...");
 $buildah_output = `buildah images --json $tmp_container 2>&1`;
 $rc = $? >> 8;
 if ($rc == 0) {
-    print "Creation of container image '$tmp_container' is complete.  Here are some details about your new image:\n";
-    print "\n$buildah_output\n";
+    logger('info', "succeeded\n");
+    logger('info', "\n$buildah_output\n");
     exit 0;
 } else {
-    print STDERR "STDERR: Could not get container image information for '$tmp_container'!\n";
-    print STDERR "        Something must have gone wrong that I don't understand.\n";
-    print STDERR "        output: $buildah_output\n";
+    logger('info', "failed\n");
+    logger('error', $buildah_output);
+    logger('error', "Could not get container image information for '$tmp_container'!  Something must have gone wrong that I don't understand.\n");
     exit 10;
 }
